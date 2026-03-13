a8295c060519d1c0b4837e1867a18e546484a830
yashsinghal23 <2024uec1486@mnit.ac.in>
2026-03-13 17:56:55 +0000

    operation

---
+++ basics/operation.js
@@ -0,0 +1,30 @@
+console.log("1"+2);
+console.log(1+2+"3");
+console.log("1"+"2");
+console.log("hello"+" yash");
+console.log(+true) //direct way of boolean to number
+
+let number=100
+let num1=++number
+let num2=number++
+
+console.log(number)
+console.log(num1);
+console.log(num2);
+
+
+console.log("2">1) // operator diectly convert the string to number
+console.log(2>1)
+console.log("abc">1)
+
+console.log(null==0)
+console.log("2"==2) //true
+console.log(null>=0) 
+
+console.log("2"===2) //=== is used for strict checking//
+/**
+ == and >= work differently (null is not convert to 0 in == while in >= convert to 0)
+ */
+
+
+
