4a71867472fe39f8013145e43e4540cea21e590f
yashsinghal23 <2024uec1486@mnit.ac.in>
2026-03-13 16:34:31 +0000

    constant

---
+++ basics/const.js
@@ -0,0 +1,14 @@
+const name= "Yash"
+var Email ="yash@01.com"
+let Password="12345678"
+usercity="JAIPUR"
+
+let userState;
+
+/**
+ use of let for variable and const for constant 
+ not use of var due to scope error
+ */
+
+Password="76tu6712"
+console.table([name,Email,Password,usercity,userState])
