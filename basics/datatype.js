8916343c7279cf5691b239b70e408a8ec85326c1
yashsinghal23 <2024uec1486@mnit.ac.in>
2026-03-13 17:26:37 +0000

    data

---
+++ basics/datatype.js
@@ -0,0 +1,61 @@
+let name="yash"
+let age=19
+let number="900234190"
+
+console.log(name);
+console.log(typeof name);
+
+console.log(age);
+console.log(typeof age);
+
+console.log(number);
+console.log(typeof number);
+
+let state=null;
+let city=undefined;
+
+console.log(typeof state); // null data type is object
+console.log(typeof city);  // undefined has a special type of data type undefined
+
+
+let score="33"
+console.log(typeof score);
+let numScore=Number(score)
+console.log(typeof numScore);
+console.log(numScore);
+
+
+let bkl=undefined
+let boolbkl=Boolean(bkl)
+
+console.log(boolbkl);
+
+let abc=33
+let stringAbc=String(abc)
+
+console.log(stringAbc);
+console.log(typeof stringAbc);
+
+
+/**
+ if string to number
+ "33" 33 output
+ "33asb" NaN output (not a number)
+ " null" give 0 output
+ "undefined" NaN
+ */
+
+ /*
+  number/string to boolean
+  1-> true 0->false
+  ""-> ,null,undefined false
+  " if sonething is written" ->true
+  */
+
+
+
+
+
+
+
+
