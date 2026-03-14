f753325abe100dc8330d09025a63d028311142a7
yashsinghal23 <2024uec1486@mnit.ac.in>
2026-03-14 17:31:52 +0000

    math

---
+++ basics/number_maths.js
@@ -0,0 +1,22 @@
+let num=new Number(1100.23)
+
+console.log(num.toFixed(1));
+console.log(num.toLocaleString('en-IN'))
+console.log(num.toString().length);
+
+
+console.log(Math.round(4.3))
+console.log(Math.min(2,4,89))
+
+console.log(Math.random()); //give value always between 0 and 1
+
+const max=20
+const min=10
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
+
+
+
+
